410a791bebde104054f4adfb009581229dce203f
Guillaume A <mail@guillaumea.fr>
2023-07-09 15:24:31 +0800

    variable fixes

--- build-docker.sh
+++ build-docker.sh
@@ -3,5 +3,5 @@ set -a
 source config.env
 set +a
 
-docker build --build-arg USER="$MY_USER" --build-arg UID="$USER_ID" --build-arg GID="$GROUP_ID" --build-arg SSH_PUBLIC_KEY="$SSH_PUBLIC_KEY" -t boostcli-container .
+docker build --build-arg MY_USER="$MY_USER" --build-arg USER_ID="$USER_ID" --build-arg GROUP_ID="$GROUP_ID" --build-arg SSH_PUBLIC_KEY="$SSH_PUBLIC_KEY" -t boostcli-container .
 
